487924d560eb1f0c704d22570abbf26aaf3b2f92
BugHunter-X <askzenai@gmail.com>
2024-12-21 22:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example of Tailwind CSS classes that might lead to unexpected behavior
+<div class="bg-red-500 hover:bg-blue-700 focus:bg-green-500">
+  This is a div with multiple hover and focus states.
+</div>
+```
